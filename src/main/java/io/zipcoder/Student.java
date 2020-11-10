3cba85bea7f9be9ec3d0f29f59a665af0f7b9973
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-09 21:57:58 -0500

    Student changes

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,79 @@
 package io.zipcoder;
 
+//import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.ArrayList;
+
+
 public class Student {
+
+
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Student(String firstName ,String lastName,Double[] testScores)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        examScores=new ArrayList<>();
+        for(Double d:testScores)
+        {
+            examScores.add(d);
+        }
+
+    }
+public Student() {
+
+}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfExamsTaken(Student s)
+    {
+      return s.examScores.size();
+
+    }
+ public String getExamScores()
+ {
+     StringBuilder str= new StringBuilder();
+      str.append("Exam Scores:\n");
+      int counter=1;
+      for (int i = 0; i < examScores.size(); i++) {
+          String strFormat= String.format("%1.0f", examScores.get(i));
+           str.append("    Exam " + counter + " -->  " + strFormat+ "\n");
+           counter++;
+
+     }
+
+     return str.toString();
+ }
+
+
+
+ public void addExamScore(double examScore)
+ {
+    // Integer addExam = examScores.size() +1;
+     examScores.add(examScore);
+   //  this.getExamScores();
+ }
+
+ public void setExamScore(int examNumber,double newScore)
+ {
+     examScores.set(examNumber,newScore);
+
+ }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,55 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
 public class StudentTest {
+    private static final Logger LOGGER= Logger.getLogger("test'");
+          //  ;//= Logger.getLogger(StudentTest.class.getName());
+
+    @Test
+    public void getExamScoresTest() {
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = {100.0, 95.0, 123.0, 96.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.getExamScores();
+
+        // Then
+        LOGGER.info("\n" + output);
+    }
+
+    @Test
+    public void getAddExamScoresTest() {
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.addExamScore(100.0);
+        String output = student.getExamScores();
+
+        // Then
+        LOGGER.info("\n" + output);
+    }
+
+    @Test
+    public void setExamScoreTest() {
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = {100.0 ,99.5};
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        student.setExamScore(1,88.5);
+        String output = student.getExamScores();
+
+        // Then
+        LOGGER.info("\n" + output);
+    }
 
 }
